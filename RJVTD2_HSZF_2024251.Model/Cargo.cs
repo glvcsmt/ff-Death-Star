9d85387003bdb100ebb20fe08a163878a19e2cf1
glvcsmt <mategalavics@gmail.com>
2024-11-08 17:20:04 +0100

    Base classes for the database.

---
+++ RJVTD2_HSZF_2024251.Model/Cargo.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RJVTD2_HSZF_2024251.Model;
+
+public enum RiskLevel //Enum to limit RiskLevel to predefined values
+{
+    Low = 0,
+    Medium = 1,
+    High = 2
+}
+
+public class Cargo  //Represents the individual cargo items 
+{
+    public Cargo(string cargoType, int imperialCredits, bool insurance, RiskLevel riskLevel, string shipmentId)
+    {
+        CargoType = cargoType;
+        ImperialCredits = imperialCredits;
+        Insurance = insurance;
+        RiskLevel = riskLevel;
+        ShipmentId = shipmentId;
+    }
+
+    public Cargo()
+    {
+        
+    }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; } //Primary key for the Cargo
+    
+    [StringLength(50)]
+    [Required]
+    public string CargoType { get; set; }   //The name of the cargo type
+    
+    [Required]
+    public int ImperialCredits { get; set; }    //The value of the cargo in Imperial Credits
+    
+    [Required]
+    public bool Insurance { get; set; } //Does the cargo have insurance? 
+    
+    [Required]
+    public RiskLevel RiskLevel { get; set; } //The risk level of the cargo
+    
+    public string ShipmentId { get; set; } //Foreign key to Shipment
+    
+    [Required]
+    public Shipment Shipment { get; set; }  //Navigation property to Shipment
+}
---
+++ RJVTD2_HSZF_2024251.Model/Shipment.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RJVTD2_HSZF_2024251.Model;
+
+public class Shipment   //Represents the detail information of a shipment
+{
+    //Constructor
+    public Shipment(string shipType, DateTime shipmentDate, CargoCapacity cargoCapacity, string status, string imperialPermitNumber, Crew crew)
+    {
+        ShipType = shipType;
+        ShipmentDate = shipmentDate;
+        CargoCapacity = cargoCapacity;
+        Status = status;
+        ImperialPermitNumber = imperialPermitNumber;
+        Crew = crew;
+        Cargoes = new HashSet<Cargo>();
+    }
+    
+    //Empty Constructor
+    public Shipment()
+    {
+        Cargoes = new HashSet<Cargo>();
+    }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; }     //Primary key for the database and the Shipment
+    
+    [StringLength(100)]
+    [Required]
+    public string ShipType { get; set; }    //The type of the ship
+    
+    [Required]
+    public DateTime ShipmentDate { get; set; }  //The time of the shipment
+    
+    [Required]
+    public CargoCapacity CargoCapacity { get; set; }    //The cargo capacity of the ship
+    
+    [StringLength(100)]
+    [Required]
+    public string Status { get; set; }  //The current status of the shipment
+    
+    [StringLength(100)]
+    [Required]
+    public string ImperialPermitNumber { get; set; }    //The Imperial Permit Number of the shipment
+    
+    [Required]
+    public Crew Crew { get; set; }  //The crew of the ship
+    
+    [Required]
+    public ICollection<Cargo> Cargoes { get; set; } //The cargoes that make up the shipment 
+}
